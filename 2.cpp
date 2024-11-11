bb3fe01c7d573ee936bef86baf8a4fe5c6cc8016
Haryce <148765923+Haryce@users.noreply.github.com>
2024-11-11 15:18:53 +0900

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <stdlib.h> 
+#include <time.h> 
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введите размер матрицы (n): ";
+    cin >> n;
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = rand() % 100;
+        }
+    }
+    cout << "Матрица:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int max = -1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if ((i >= n / 4 && i <= 3 * n / 4) && (j >= n / 4 && j <= 3 * n / 4)) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+    }
+
+    cout << "Максимальное значение в темных зонах: " << max << endl;
+}
